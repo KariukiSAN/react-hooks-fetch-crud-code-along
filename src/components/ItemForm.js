0ae48f8b4a9d65d916db2e42a9a3ec8e05c614c0
Annette Kariuki <kariukian1@gmail.com>
2023-09-15 20:50:51 +0300

    Complete

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,45 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+
+//Our next goal will be to add a new item to our database on the server when a user submits the form.
+//Mind map of this goal is:
+//When a user submits the form= event X
+//Make Y fetch request (POST /items with the new item data)
+//Update Z state (add a new item to state)
+
+
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
+  
+  
+  //Adding a function to handle our submissions.
+  function handleSubmit(e){
+    e.preventDefault();
+
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    
+    const postItem = ()=>{
+      fetch("http://localhost:4000/items",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify(itemData),
+      })
+      .then(resp => resp.json())
+      .then(item => onAddItem(item))
+    }
+
+    postItem()
+  }
 
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,51 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  const fetchItems = ()=>{
+    fetch("http://localhost:4000/items")
+    .then(resp=>resp.json())
+    .then(items=> {
+      setItems(items)
+    })
+  }
+
+  //Adding a useEffect hook to initiate our fetch request.
+  // Update state by passing the array of items to setItems 
+
+
+  //mind map of events:
+  //-When X event occurs, the useEffect hook is used to trigger a SIDE EFFECT in the ShoppingList component. 
+  //-This is after the component first renders.
+  //-then make Y ie fetch request to retrieve items list.
+  //-then we need to update a Z state so as to replace our original list with our new list.
+
+
+  useEffect(
+    ()=>fetchItems(),
+    []
+  )
+
+  function handleAddItem(newItem){
+    setItems([...items,newItem])
+  }
+
+  function handleUpdateItem(updatedItem) {
+
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems)
+  }
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +64,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
